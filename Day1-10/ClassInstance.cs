e33ef55a22fd1999c95916726bc599317af962c7
Jackie <JGtheophila19@gmail.com>
2019-09-26 12:30:43 -0400

    passed 1 out of 3 test cases
     - continue to work on it...

--- Day1-10/ClassInstance.cs
+++ Day1-10/ClassInstance.cs
@@ -5,21 +5,37 @@ using System.IO;
 class Person {
     public int age;     
 	public Person(int initialAge) {
-        // Add some more code to run some checks on initialAge
-        // If age < 13, print You are young.
-        // If  and age > 13, age < 18, print You are a teenager.
-        // Else, print You are old.
+	   if(initialAge >= 0)
+           {
+               age = initialAge;
+           }
+           
+	   else
+           {
+               Console.WriteLine("Age is not valid, setting age to 0.");
+           }
      }
      public void amIOld() {
         // Do some computations in here and print out the correct statement to the console 
-        Console.WriteLine("Age is not valid, setting age to 0..");
-        Console.WriteLine("You are young.");
-        Console.WriteLine("You are teenager.");
-        Console.WriteLine("You are old.");
+        if(age < 13)
+        {
+	   Console.WriteLine("You are young.");
+	}
+        
+	else if(age >= 13 && age < 18)
+        {
+	   Console.WriteLine("You are teenager.");
+	}
+       
+	else
+        {
+	   Console.WriteLine("You are old.");
+	}
      }
 
      public void yearPasses() {
         // Increment the age of the person in here
+	     age++;
      }
 
 static void Main(String[] args) {
