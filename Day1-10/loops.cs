7b6c6b8c0ad1fcda2f8587b60205b043a389235b
Jackie <JGtheophila19@gmail.com>
2019-09-27 19:09:13 -0400

    passed 1 out of 3 test case
    - must use for loop, not manually

---
+++ Day1-10/loops.cs
@@ -0,0 +1,29 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+    static void Main(string[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("2 x 1 = " + n * 1);
+        Console.WriteLine("2 x 2 = " + n * 2);
+        Console.WriteLine("2 x 3 = " + n * 3);
+        Console.WriteLine("2 x 4 = " + n * 4);
+        Console.WriteLine("2 x 5 = " + n * 5);
+        Console.WriteLine("2 x 6 = " + n * 6);
+        Console.WriteLine("2 x 7 = " + n * 7);
+        Console.WriteLine("2 x 8 = " + n * 8);
+        Console.WriteLine("2 x 9 = " + n * 9);
+        Console.WriteLine("2 x 10 = " + n * 10);
+    }
+}
